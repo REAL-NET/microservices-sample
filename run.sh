cea069386f0e5f1ef480d299174b3830258fa5ae
Mikhail Kidyankin <kidyankin@icloud.com>
2019-11-06 13:34:24 +0300

    Add running without git

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$BRANCH" == "" ]]; then
+    BRANCH="master"
+fi
 echo "Running services for branch $BRANCH"
 
 docker run -p 8000:80 docker.pkg.github.com/real-net/microservices-sample/hello:$BRANCH &
